dc3629413a32219eb2daabe8e8b357a87bf12f01
PizzaMyHeart <ian_tan@hotmail.com>
2021-12-31 23:08:42 +0000

    change feature svg images

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -5,16 +5,16 @@ import styles from './HomepageFeatures.module.css';
 const FeatureList = [
   {
     title: 'Easy to Use',
-    Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
+    Svg: require('../../static/img/undraw_search_re_x5gq.svg').default,
     description: (
       <>
-        Guidelines that are easy to find and read. 
+        Guidelines that are easy to find and read, complete with nifty search bar.
       </>
     ),
   },
   {
     title: 'Easy to Write',
-    Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
+    Svg: require('../../static/img/undraw_typewriter_re_u9i2.svg').default,
     description: (
       <>
         Netlify CMS lets you write documents in Markdown or rich text.
@@ -23,7 +23,7 @@ const FeatureList = [
   },
   {
     title: 'Online or Offline',
-    Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
+    Svg: require('../../static/img/undraw_in_sync_re_jlqd.svg').default,
     description: (
       <>
         Want to browse Bart as a website on your PC? Or use it as an offline app you can install on your device? You get to decide.
